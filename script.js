d2caa62eb73ae75843b2cc3af1bceea38432ac92
takumi0623eve <acua0623@gmail.com>
2022-01-12 23:44:32 +0900

    Added dummy firnds site data

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    var pagetop = $('#page_top');
+    // ボタン非表示
+    pagetop.hide();
+    var wid = $(window).width();
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
